b07458f15bab80a8f5d8f111973ed5480545e0b4
RicV-Sam <riccardo.vallaro@gmail.com>
2026-06-23 11:17:49 +0100

    Clean Turkey all-inclusive encoding artifacts

--- scripts/seo_consistency_audit.py
+++ scripts/seo_consistency_audit.py
@@ -8,6 +8,7 @@ ROOT = Path(__file__).resolve().parents[1]
 MOJIBAKE_GUARDED_PAGES = {
     "guides/best-travel-booking-websites-uk/index.html",
     "guides/cheap-holidays-mauritius-from-uk/index.html",
+    "guides/cheap-holidays-turkey-all-inclusive-from-uk/index.html",
     "make-money-for-travel/how-to-make-500-for-travel-2026.html",
 }
 MOJIBAKE_CODEPOINT_MARKERS = tuple(chr(codepoint) for codepoint in (0x00C2, 0x00E2, 0x00F0))
