328a6ae571a90f1c08ad1585c191ed17843c04e2
RicV-Sam <riccardo.vallaro@gmail.com>
2026-06-23 11:28:46 +0100

    Clean Seychelles page encoding artifacts

--- scripts/seo_consistency_audit.py
+++ scripts/seo_consistency_audit.py
@@ -9,6 +9,7 @@ MOJIBAKE_GUARDED_PAGES = {
     "guides/best-travel-booking-websites-uk/index.html",
     "guides/cheap-holidays-bali-from-uk/index.html",
     "guides/cheap-holidays-mauritius-from-uk/index.html",
+    "guides/cheap-holidays-seychelles-from-uk/index.html",
     "guides/cheap-holidays-thailand-from-uk/index.html",
     "guides/cheap-holidays-turkey-all-inclusive-from-uk/index.html",
     "make-money-for-travel/how-to-make-500-for-travel-2026.html",
